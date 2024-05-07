1e6e26a29f42faaae4a548e768dc38c494e53c12
Daniel Pena <163460072+daperk@users.noreply.github.com>
2024-05-07 01:04:59 +0000

    cards

--- src/app.js
+++ src/app.js
@@ -1,11 +1,38 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  let card = document.querySelector(".Card");
+  card.classList.add(generaterandomsuit());
+  card.innerHTML = generaterandomnumber();
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generaterandomnumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+
+  return numbers[indexNumbers];
+};
+
+let generaterandomsuit = () => {
+  let suits = ["heart", "spade", "club", "diamond"];
+
+  let indexSuit = Math.floor(Math.random() * suits.length);
+
+  return suits[indexSuit];
 };
